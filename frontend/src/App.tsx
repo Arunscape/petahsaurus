dad94a03483e3ce8f4dc5b454e631643a87e460d
Jacob Reckhard <jacobreckhard@gmail.com>
2021-01-16 14:09:59 -0700

    started signin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect, useHistory } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound'
-
+import SignIn from './pages/SignIn';
 import useGlobalState from './useGlobalState';
 
 // import logo from './logo.svg';
@@ -19,14 +19,16 @@ const App = ({}: AppProps) => {
     const timer = setTimeout(() => setCount(count + 1), 1000);
     return () => clearTimeout(timer);
   }, [count, setCount]);
+
   // Return the App component.
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Redirect to="/home"/>
+          <Redirect to="/signin"/>
         </Route>
       <Route exact path="/home" component={Home}/>
+      <Route exact path="/signin" component={SignIn}/>
       <Route component={NotFound}/>
       </Switch>
     </Router>
--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,5 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const SignIn = () => <div>This is the sign in page =D</div>
+const Frame = (props) =>
+    <div>
+        {props.children}
+    </div>
+
+enum State {
+    GetEmail,
+    Signin,
+    Signup,
+    Validating,
+    Complete,
+}
+
+const SignIn = () => 
+    const [state, setState] = useState(State.GetEmail);
+    <Frame>
+        <p>This is the sign in page =D</p>
+    </Frame>
+
+
+
 
 export default SignIn;
