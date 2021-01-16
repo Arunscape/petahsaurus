feeb0f6c3f8dde717f5328a353f46fdf1142c7b8
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-01-16 13:44:59 -0700

    hamburger menu is messed up lol

--- frontend/src/components/Findings.tsx
+++ frontend/src/components/Findings.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-interface props {
+interface FindingsProps {
     image: string,
     description: string,
 }
 
-const Findings = (props) => <div>
+const Findings = (props: FindingsProps) => <div>
     <img src="https://via.placeholder.com/150" />
     <p>this is a description for the component</p>
 </div>
---
+++ frontend/src/components/HamburgerButton.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components';
+
+export type ButtonComponentProps = {
+  children?: HTMLCollection | string,
+  onClick: (e?: React.MouseEvent) => void
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const StyledBurger = styled.button<ButtonComponentProps>`
+  position: absolute;
+  top: 5%;
+  left: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 2rem;
+  height: 2rem;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  z-index: 10;
+  
+  &:focus {
+    outline: none;
+  }
+  
+  div {
+    width: 2rem;
+    height: 0.25rem;
+    background: ${({ theme }) => theme.primaryLight};
+    border-radius: 10px;
+    transition: all 0.3s linear;
+    position: relative;
+    transform-origin: 1px;
+  }
+  border-color: coral;
+`;
+
+
+const HamburgerButton = () => <StyledBurger>
+    <div>
+      WHYYYYYY
+    </div>
+    <div/>
+    <div/>
+</StyledBurger>
+export default HamburgerButton;
