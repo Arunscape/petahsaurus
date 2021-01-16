889776f2cd40fb7b69b6a450693f14e63cb3ec55
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-01-16 13:14:22 -0700

    garros you can start styling stuff now

---
+++ frontend/src/components/Findings.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface props {
+    image: string,
+    description: string,
+}
+
+const Findings = (props) => <div>
+    <img src="https://via.placeholder.com/150" />
+    <p>this is a description for the component</p>
+</div>
+
+
+export default Findings;
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+color: red;
+`;
+
 import HamburgerMenu from './HamburgerMenu'
 import Search from './Search';
+
 
 const Header = () => {
 
