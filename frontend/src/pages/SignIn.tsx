d6ca811e608eb49c6628ec7818b9e7056e816b77
Jacob Reckhard <jacobreckhard@gmail.com>
2021-01-16 15:22:44 -0700

    Revert "pulled these changes out of the reflog, cause somebody removed them"
    This reverts commit 980e8ab6f1888bc88cfb96c7d71623ab15004fcc.

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,5 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
+import * as Api from '../api'
 
-const SignIn = () => <div>This is the sign in page =D</div>
+const Frame = (props) =>
+    <div>
+        {props.children}
+    </div>
+    
+
+enum State {
+    GetEmail,
+    Signin,
+    Signup,
+    Validating,
+    Complete,
+}
+const bindInputToState = (setter) => (event) => setter(event.target.value)
+
+const checkEmail = (email) => {
+    console.log(email);
+    const promis = Api.checkEmail(email);
+}
+
+
+const SignIn = () => {
+    const [state, setState] = useState(State.GetEmail);
+    const [email, setEmail] = useState("");
+    switch (state) {
+        case State.GetEmail:
+            return (
+                <Frame>
+                    <p>Getting email</p>
+                    <input type="email" placeholder="email@example.com" onChange={bindInputToState(setEmail)}/>
+                    <button onClick={() => checkEmail(email)}>Next</button>
+                </Frame>
+                )
+            break;
+        case State.Signin:
+            return (
+                <Frame>
+                    <p>Signin</p>
+                </Frame>
+                )
+            break;
+        case State.Signup:
+            return (
+                <Frame>
+                    <p>Signup</p>
+                </Frame>
+                )
+            break;
+        case State.Validating:
+            return (
+                <Frame>
+                    <p>Validating</p>
+                </Frame>
+                )
+            break;
+        case State.Complete:
+            return (
+                <Frame>
+                    <p>Done</p>
+                </Frame>
+                )
+            break;
+        default:
+            return (<Frame>
+                    <p>An error has occured :(</p>
+            </Frame>)
+            break;
+    }
+}
+
+
+
 
 export default SignIn;
