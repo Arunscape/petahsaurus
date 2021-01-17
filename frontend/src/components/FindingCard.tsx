ceec8c8cc7108473d9bc878c7492c34d7f270986
Jacob Reckhard <jacobreckhard@gmail.com>
2021-01-17 01:47:22 -0700

    styled cards

--- frontend/src/components/FindingCard.tsx
+++ frontend/src/components/FindingCard.tsx
@@ -3,7 +3,19 @@ import type { Finding } from 'src/api';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom'
 
-const Card = styled.div``;
+const Card = styled.div`
+  background-color: white;
+  width: 80vw;
+  margin-left: 10vw;
+  border-radius: 10px;
+  box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 50px;
+  padding-bottom: 50px;
+`;
 const StyledImg = styled.img``;
 const Description = styled.p``;
 
