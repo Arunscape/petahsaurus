bbc6c49babde9eb4d5dc8d1283ee92e0c7fcc564
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-01-17 02:25:59 -0700

    add map route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, Redirect, useHistory } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+} from 'react-router-dom';
 import Home from './pages/Home';
 import NewFinding from './pages/NewFinding';
-import NotFound from './pages/NotFound'
+import NotFound from './pages/NotFound';
 import SignIn from './pages/SignIn';
-import SearchPage from  './pages/SearchPage'
-import * as Api from './api'
+import SearchPage from './pages/SearchPage';
+import * as Api from './api';
 import FindingsPage from './pages/FindingsPage';
 // import logo from './logo.svg';
 // import './App.css';
@@ -13,7 +20,6 @@ import FindingsPage from './pages/FindingsPage';
 interface AppProps {}
 
 const App = ({}: AppProps) => {
-
   //Api.createFinding({content: "eat shit", coords: {lat: 5.5, long: 5.5}})
   //    .then((resp) => Api.getFinding(resp.data.id))
   //    .then((data) => console.log(data.data));
@@ -23,24 +29,30 @@ const App = ({}: AppProps) => {
   return (
     <Router>
       <Switch>
-      <Route exact path="/" component={() => {
-        const history = useHistory();
-        Api.upgrade()
-          .then(() => history.push('/home'))
-          .catch(() => history.push('/signin'));
+        <Route
+          exact
+          path="/"
+          component={() => {
+            const history = useHistory();
+            Api.upgrade()
+              .then(() => history.push('/home'))
+              .catch(() => history.push('/signin'));
 
-        return <div>loading...</div>;
-      }}/>
-      <Route exact path="/add" component={NewFinding}/>
-      <Route exact path="/home" component={Home}/>
-      <Route exact path="/signin" component={SignIn}/>
-      <Route exact path="/search" component={SearchPage}/>
-      <Route path="/finding" component={FindingsPage}/> {/*route is not exact*/}
-      <Route path="/edit" component={NewFinding}/> {/*route is not exact*/}
-      <Route component={NotFound}/>
+            return <div>loading...</div>;
+          }}
+        />
+        <Route exact path="/add" component={NewFinding} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/map" component={Home} />
+        <Route exact path="/signin" component={SignIn} />
+        <Route exact path="/search" component={SearchPage} />
+        <Route path="/finding" component={FindingsPage} />{' '}
+        {/*route is not exact*/}
+        <Route path="/edit" component={NewFinding} /> {/*route is not exact*/}
+        <Route component={NotFound} />
       </Switch>
     </Router>
   );
-}
+};
 
 export default App;
