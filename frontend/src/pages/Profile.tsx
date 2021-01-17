d4ef50e87b6603e4f9c0cb9a313a08871c3a3f34
Christiana Garros <christianawgarros@gmail.com>
2021-01-17 11:47:48 -0700

    added a friend

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import * as Api from '../api';
 import Header from '../components/Header';
 import styled from 'styled-components';
+import friend from '../assets/rptr.png'
 
 const StyledBackground = styled.div`
 display: flex;
@@ -9,11 +10,18 @@ display: flex;
   justify-content: flex-start;
   align-items: center;
   align-content: center;
+  font-family: 'Open Sans';
+`;
+
+const Friend = styled.img`
+width: 30vw;
+min-width: 20rem;
 `;
 
 const Profile = () => {
     return <StyledBackground>
         <Header></Header>
+        <Friend src={friend} />
         <p>Your Profile :)</p>
         <span>Email: </span><span>{Api.getUserIdInfo().email}</span>
         </StyledBackground>
