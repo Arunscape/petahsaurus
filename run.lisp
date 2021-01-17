a4a90c497afec75e4bdadb8a3e4db5f1bace85f9
Jacob Reckhard <jacobreckhard@gmail.com>
2021-01-16 17:39:32 -0700

    fixed run

--- run.lisp
+++ run.lisp
@@ -1,12 +1,6 @@
 #!/usr/bin/sbcl --script
-
-;;; The following lines added by ql:add-to-init-file:
-#-quicklisp
-(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
-                                       (user-homedir-pathname))))
-  (when (probe-file quicklisp-init)
-    (load quicklisp-init)))
+(load "~/.sbclrc")
 
 (ql:quickload '(clack petahsaurus))
-(load "~/.emacs.d/elpa/slime-20191129.1304/start-swank.lisp")
+(when *swankpath* (load *swankpath*))
 (clack:clackup petahsaurus.api:*static-app* :use-thread nil)
