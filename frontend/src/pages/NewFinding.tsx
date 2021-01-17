e094f40252b24fbacb96c7c81613cbc3ea0470e8
Peter Elliott <pelliott@ualberta.ca>
2021-01-16 21:38:09 -0700

    fix description in newfinding

--- frontend/src/pages/NewFinding.tsx
+++ frontend/src/pages/NewFinding.tsx
@@ -15,14 +15,14 @@ function TagEditor(props) {
         if (i === index) {
             return [e.target.value, element[1]]
         } else {
-            return element; 
+            return element;
         }
     }))}/>
         <input type="text" onChange={(e) => setter(props.original.map((element: [string, string], i:number) => {
         if (i === index) {
             return [element[0], e.target.value]
         } else {
-            return element; 
+            return element;
         }
     }))}/>
     </div>
@@ -36,31 +36,23 @@ const NewFindings = () => {
     const [picture, setPicture] = useState(null);
     const inputRef = useRef();
     const history = useHistory();
-    
+
     const submitFunction = async () => {
-        //console.log("Submit got clicked");
-        //console.log(inputRef)
-        //console.log(picture)
-        //console.log(descr);
-        //console.log(tagKey + " " + tagVal);
         const reader = new FileReader();
 
         reader.addEventListener("load", function () {
-          // convert image file to base64 string
-          //console.log(reader.result.toString());
           setPicture(reader.result.toString())
 
-          Api.createFinding({content: "eat shit", coords: {lat: 5.5, long: 5.5}, image: reader.result.toString(), date: 69})
-              //.then((resp) => Api.getFinding(resp.data.id))
+          Api.createFinding({content: descr, coords: {lat: 5.5, long: 5.5}, image: reader.result.toString(), date: 69})
               .then((data) => {
                 for (let [key, value] of tagList) {
                     console.log("posting for tags: " + key + value);
                     Api.setTag(data.data.id, key, value);
-                }  
+                }
                 });
               history.push("/home");
         }, false);
-      
+
         if (picture) {
           reader.readAsDataURL(picture);
           setPicture(reader.result.toString())
@@ -69,10 +61,10 @@ const NewFindings = () => {
 
     return <div>
         <Header/>
-        MAKE A DINOSAUR PAGE 
+        MAKE A DINOSAUR PAGE
         <p>picture</p>
         <input ref={inputRef} type="file" accept="image/x-png,image/jpeg" onChange={(e) => {
-            setPicture(e.target.files.item(0)) 
+            setPicture(e.target.files.item(0))
         // make backend call to upload picture
         }}/>
         <p>description</p>
